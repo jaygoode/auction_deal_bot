7c69849e840a2e906ff995b39f1c42cb5a84f22c
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2023-10-21 19:05:59 +0300

    added login functionailty

--- functions.py
+++ functions.py
@@ -1,18 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 
 
 def open_huutokaupat():
-    chromedriver_path = ""
-    website_address = "www.huutokaupat.fi"
-    driver = webdriver.Chrome(executable_path=chromedriver_path)
+    website_address = "http://www.huutokaupat.com"
+    driver = webdriver.Chrome()
     driver.get(website_address)
     return driver
 
 
 def login_to_huutokaupat(driver):
-    login_nav_button = driver.find_element(By.XPATH, "//div/nav/ul/li[4]")
-    pass
+    cookies_warning_popup_xpath = "//div/button[text()='Vain välttämättömät']"
+    sleep(5)
+    if is_element(driver, cookies_warning_popup_xpath):
+        driver.find_element(
+            By.XPATH, "//div/button[text()='Vain välttämättömät']"
+        ).click()
+    sleep(5)
+    login_nav_button = driver.find_element(By.XPATH, "//div/nav/ul/li[4]").click()
+    username = input("username: ")
+    pwd = input("pwd: ")
+    email_input = driver.find_element(
+        By.XPATH, "//div/input[@type = 'email']"
+    ).send_keys(username)
+    pwd_input = driver.find_element(
+        By.XPATH, "//div/input[@type = 'password']"
+    ).send_keys(pwd)
+
+    login_btn = driver.find_element(By.XPATH, "//button[text()='Kirjaudu']").click()
+
+    oma_sivu_xpath = "//span[text()='Oma sivu']"
+    if is_element(driver, cookies_warning_popup_xpath):
+        print("Login successful!")
+    else:
+        print("Login failed..")
+
+    breakpoint()
 
 
 def search_items(driver):
@@ -21,3 +46,8 @@ def search_items(driver):
 
 def add_to_following_list(driver):
     pass
+
+
+def is_element(driver, xpath):
+    elements = driver.find_elements(By.XPATH, xpath)
+    return len(elements) > 0
--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from functions import (
 
 def main():
     driver = open_huutokaupat()
-    login_to_huutokaupat()
-    search_items()
-    add_to_following_list()
+    login_to_huutokaupat(driver)
+    search_items(driver)
+    add_to_following_list(driver)
 
 
 if __name__ == "__main__":
