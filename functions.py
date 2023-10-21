e393affd1f3d83bd2c560c4adfe764110ee694fe
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2023-10-21 19:10:26 +0300

    refactored all funcs to a class

--- functions.py
+++ functions.py
@@ -1,38 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from time import sleep
 
 
-def open_huutokaupat():
-    website_address = "http://www.huutokaupat.com"
-    driver = webdriver.Chrome()
-    driver.get(website_address)
-    return driver
+class Selenium_functions:
+    def __init__(self) -> None:
+        self.website_address = "http://www.huutokaupat.com"
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.website_address)
 
 
-def login_to_huutokaupat(driver):
+def login_to_huutokaupat(self):
     cookies_warning_popup_xpath = "//div/button[text()='Vain välttämättömät']"
     sleep(5)
-    if is_element(driver, cookies_warning_popup_xpath):
-        driver.find_element(
+    if is_element(self.driver, cookies_warning_popup_xpath):
+        self.driver.find_element(
             By.XPATH, "//div/button[text()='Vain välttämättömät']"
         ).click()
     sleep(5)
-    login_nav_button = driver.find_element(By.XPATH, "//div/nav/ul/li[4]").click()
+    login_nav_button = self.driver.find_element(By.XPATH, "//div/nav/ul/li[4]").click()
+
     username = input("username: ")
     pwd = input("pwd: ")
-    email_input = driver.find_element(
+
+    email_input = self.driver.find_element(
         By.XPATH, "//div/input[@type = 'email']"
     ).send_keys(username)
-    pwd_input = driver.find_element(
+    pwd_input = self.driver.find_element(
         By.XPATH, "//div/input[@type = 'password']"
     ).send_keys(pwd)
 
-    login_btn = driver.find_element(By.XPATH, "//button[text()='Kirjaudu']").click()
+    login_btn = self.driver.find_element(
+        By.XPATH, "//button[text()='Kirjaudu']"
+    ).click()
 
+    sleep(5)
     oma_sivu_xpath = "//span[text()='Oma sivu']"
-    if is_element(driver, cookies_warning_popup_xpath):
+    if is_element(self.driver, cookies_warning_popup_xpath):
         print("Login successful!")
     else:
         print("Login failed..")
@@ -40,14 +48,14 @@ def login_to_huutokaupat(driver):
     breakpoint()
 
 
-def search_items(driver):
+def search_items(self):
     pass
 
 
-def add_to_following_list(driver):
+def add_to_following_list(self):
     pass
 
 
-def is_element(driver, xpath):
-    elements = driver.find_elements(By.XPATH, xpath)
+def is_element(self, xpath):
+    elements = self.driver.find_elements(By.XPATH, xpath)
     return len(elements) > 0
